fb76f9e07eac7cc21524aa677a55bdacb26ac531
ThanushanxCode <thanushanxcode@gmail.com>
2023-04-05 00:25:33 +0530

    Product Service Bug Fixes

--- Login/src/main/java/net/bumblebee/service/ProductService.java
+++ Login/src/main/java/net/bumblebee/service/ProductService.java
@@ -4,5 +4,13 @@ import net.bumblebee.entity.Product;
 import java.util.List;
 
 public interface ProductService {
+    List<Product> getAllProducts();
 
+    Product saveProduct(Product Product);
+
+    Product getProductById(Long id);
+
+    Product updateProduct(Product Product);
+
+    void deleteProductById(Long id);
 }
