d5cdc8d64254b65b1556924af9baafeccea2a2ef
ThanushanxCode <thanushanxcode@gmail.com>
2023-04-05 00:10:10 +0530

    Login API Bug authentication Bug Fixes

--- Login/src/main/java/net/bumblebee/config/SecurityConfiguration.java
+++ Login/src/main/java/net/bumblebee/config/SecurityConfiguration.java
@@ -25,7 +25,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
